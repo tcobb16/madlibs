1498289f72f254aa9c1ff6c7f315606c737c1868
Tessa Cobb <tcobb2162@gmail.com>
2022-08-21 15:57:42 -0700

    madlib base functionality, not further study attempted yet

---
+++ app.py
@@ -0,0 +1,25 @@
+from flask import Flask, render_template, request
+from flask_debugtoolbar import DebugToolbarExtension
+from stories import story
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = "secret"
+
+debug = DebugToolbarExtension(app)
+
+
+@app.route("/")
+def ask_for_words():
+    """show form to ask for words"""
+
+    prompts = story.prompts
+    return render_template("words.html", prompts=prompts)
+
+
+@app.route("/story")
+def show_story():
+    """Show story"""
+
+    text = story.generate(request.args)
+
+    return render_template("story.html", text=text)
